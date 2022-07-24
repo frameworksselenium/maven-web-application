c3d0605ffa8282c06d78dd0f245465e04979b0da
frameworksselenium <frameworksselenium@gmail.com>
2022-07-24 00:03:14 -0400

    initial

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -7,9 +7,13 @@ import javax.servlet.http.HttpSession;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 
 @Controller
 @RequestMapping("/employee")
@@ -19,7 +23,7 @@ public class EmployeeService {
 	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
 	@ResponseBody
 	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
-			throws JSONException {
+			throws JSONException, UnknownHostException {
 
 		JSONObject js = new JSONObject();
 		js.put("Name", "Mithun Technologies");
@@ -27,7 +31,8 @@ public class EmployeeService {
 		js.put("DOB", "08-Nov-2011");
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
 		js.put("Places he like", "His native place");
-
+		js.put("Machine IP", Inet4Address.getLocalHost().getHostAddress());
 		return js.toString();
-}
+	}
+
 }
