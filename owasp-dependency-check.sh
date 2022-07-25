faf00d6137fea1f9e61a100f03eb59eccfa34c53
frameworksselenium <frameworksselenium@gmail.com>
2022-07-25 11:56:21 -0400

    initial

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -5,7 +5,7 @@ DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
 REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
 
 if [ ! -d "$DATA_DIRECTORY" ]; then
-    echo "Initially creating persistent directories"
+    #echo "Initially creating persistent directories"
     mkdir -p "$DATA_DIRECTORY"
     chmod -R 777 "$DATA_DIRECTORY"
 
